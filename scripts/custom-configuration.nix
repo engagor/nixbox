c8cfed0911b214cd23f0cc13cd62bdbd65fd9529
Erik Van Kelst <4levels@gmail.com>
2019-08-20 15:22:41 +0200

    Update nixos-minimal-19.03 release urls Disable sandbox to prevent composer/npm/go/.. access issues

--- scripts/custom-configuration.nix
+++ scripts/custom-configuration.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
-# Place here any custom configuration specific to your organisation (locale, ...)
-# if you want it to be part of the packer base image to be used with vagrant.
+  # Place here any custom configuration specific to your organisation (locale, ...)
+  # if you want it to be part of the packer base image to be used with vagrant.
+
+  # if the sandbox is left on (default), any calls to composer / npm / go functions
+  # that need to fetch remote sources will fail
+  nix.useSandbox = false;
 }
